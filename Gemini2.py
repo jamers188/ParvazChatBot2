629450d16c841ab0c7816ece14c5d53b022cd6f9
jamers188 <123589840+jamers188@users.noreply.github.com>
2024-03-18 22:41:22 +0400

    Update Gemini2.py

--- Gemini2.py
+++ Gemini2.py
@@ -84,7 +84,10 @@ if selected == "HOME":
     <br>
     <span style='color:#222831'>  📁 Navigate to PDF CHAT to chat with the PDF'S..</span>
     <br>
-     <span style='color:#222831'> Explore the Possibilities: ParvazChatBot2 is a versatile tool that can assist you in various tasks, from answering questions to analyzing images and PDF documents. Explore its capabilities and discover new ways to leverage its intelligence for your needs. 
+     <span style='color:#222831'> Explore the Possibilities:</span>
+     <br>
+     
+     <span style='color:#222831'> ParvazChatBot2 is a versatile tool that can assist you in various tasks, from answering questions to analyzing images and PDF documents. Explore its capabilities and discover new ways to leverage its intelligence for your needs. 
      </span>
     <br>
 """, unsafe_allow_html=True)
