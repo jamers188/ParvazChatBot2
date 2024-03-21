e7f9f476db684c2bf4d3bf8241d61757fa386989
jamers188 <123589840+jamers188@users.noreply.github.com>
2024-03-21 22:26:43 +0400

    Update Gemini2.py

--- Gemini2.py
+++ Gemini2.py
@@ -15,57 +15,29 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
-
 # Load environment variables from .env file if present
 load_dotenv()
 
 API_KEY = os.environ.get("GOOGLE_API_KEY")
 genai.configure(api_key=API_KEY)
 
-
-
-
-## function to load Gemini Pro model and get responses
+# Function to load Gemini Pro model and get responses
 model = genai.GenerativeModel("gemini-pro")
 chat = model.start_chat(history=[])
 
-from streamlit_option_menu import option_menu
- ##initialize our streamlit app
-st.set_page_config(page_title="MyAI", page_icon='🤖')  # page title
-#with st.sidebar:
-
-
-
-# Use option_menu with the defined styles
-selected = option_menu(
-    menu_title=None,
-    options=["HOME","Prompt Chat", "IMAGE CHAT" ,"PDF CHAT","CHAT HISTORY"],
-    icons=['house',"pen" ,'image','book','chat','person'],
-    default_index=0,
-    menu_icon='user',
-    orientation="horizontal",
-    styles="""
-    <style>
-        .option-menu {
-            width: 200px; /* Set the desired width */
-            margin-right: 20px; /* Set the desired spacing */
-        }
-    </style>
-"""
-)
-    # Initialize session state for chat history,image history,pdf history if it doesn't exist
+# Initialize session state for chat history, image history, pdf history if it doesn't exist
 if 'chat_history' not in st.session_state:
-        st.session_state['chat_history'] = []
+    st.session_state['chat_history'] = []
 if 'img_history' not in st.session_state:
-        st.session_state['img_history'] = []
-if  'img_srchistory' not in  st.session_state:
-        st.session_state['img_srchistory'] = []
-if  'pdf_history' not in  st.session_state:
-        st.session_state['pdf_history'] = []
-if  'pdf_srchistory' not in  st.session_state:
-        st.session_state['pdf_srchistory'] = []
+    st.session_state['img_history'] = []
+if 'img_srchistory' not in st.session_state:
+    st.session_state['img_srchistory'] = []
+if 'pdf_history' not in st.session_state:
+    st.session_state['pdf_history'] = []
+if 'pdf_srchistory' not in st.session_state:
+    st.session_state['pdf_srchistory'] = []
 
- # Displaying the home page content
+# Displaying the home page content
 if selected == "HOME":
     st.markdown("""# <span style='color:#0A2647'> Welcome to My Streamlit App  ** MyAI 🦅</span>""", unsafe_allow_html=True)
 
@@ -92,19 +64,16 @@ if selected == "HOME":
      </span>
     <br>
 """, unsafe_allow_html=True)
- # Function to get responses from the Gemini chatbot
+
+# Function to get responses from the Gemini chatbot
 if selected == "Prompt Chat":
     def get_gemini_response(question):
         response = chat.send_message(question, stream=True)
-       # response = model.generate_content(input_text)
         return response
-
-
-
 
     input_text = st.chat_input("Ask your Question")
     
-    if  1 and input_text:
+    if input_text:
         response = get_gemini_response(input_text)
         
         st.session_state['chat_history'].append(("YOU", input_text))
@@ -133,18 +102,16 @@ if selected == "Prompt Chat":
                     st.warning("Invalid response format. Unable to extract text from parts.")
         else:
             st.warning("Invalid response format. No parts found.")
- # Displaying the chat history
+
+# Displaying the chat history
 if selected == 'CHAT HISTORY':
     st.title("CHAT HISTORY")
     
-   
-    
-    # Create two columns for buttons
+    # Create three columns for buttons
     text_history_button, image_history_button, pdf_history_button = st.columns([1, 1, 1])
+    
     # Adjust column ratios as needed
-
     with text_history_button:
-
         # Display chat history for text if the button is clicked
         if st.button("Show Text Chat History", use_container_width=True):
             if 'chat_history' in st.session_state and st.session_state['chat_history']:
@@ -156,8 +123,9 @@ if selected == 'CHAT HISTORY':
                         st.markdown(f"**{role} 🤖**: {text} ")
             else:
                 st.error("Text Chat History is empty. Start asking questions to build the history.")
-  # Display image chat history if the button is clicked
+  
     with image_history_button:
+        # Display image chat history if the button is clicked
         if st.button("Show Image Chat History", use_container_width=True):
             for history_type, header_text, emoji in [
                 ('img_history', "Image Chat History:", "👤"),
@@ -174,13 +142,12 @@ if selected == 'CHAT HISTORY':
                 else:
                     st.error(error_message)
 
-
-        
     with pdf_history_button:
         # Display pdf history for image if the button is clicked
         if st.button("Show PDF Chat History", use_container_width=True):
             for history_type in ['pdf_history', 'pdf_srchistory']:
                 if history := st.session_state.get(history_type):
+                    title
                     title = "PDF Chat History:" if history_type == 'pdf_history' else "PDF Source History:"
                     st.subheader(title)
                     for role, user_text in history:
@@ -189,11 +156,7 @@ if selected == 'CHAT HISTORY':
                 else:
                     st.error(f"{history_type.capitalize()} is empty. Start asking questions with PDFs to build the history.")
 
-                    
-
     st.warning("THE CHAT HISTORY WILL BE LOST ONCE THE SESSION EXPIRES")
-
-        
 
 if selected == "IMAGE CHAT":
     vision_model = genai.GenerativeModel('gemini-pro-vision')
@@ -202,10 +165,7 @@ if selected == "IMAGE CHAT":
         response = vision_model.generate_content([input_text_1, image], stream=True)
         return response
     
-    
-
     # Option to choose between file upload and URL input
-
     option = st.radio("Choose an option", ["Upload Image", "Provide Image URL"])
 
     if option == "Upload Image":
@@ -216,8 +176,7 @@ if selected == "IMAGE CHAT":
                 st.image(image, caption='Uploaded Image', use_column_width=True)
                 st.session_state['img_srchistory'].append(("SOURCE", option))
             except Exception as e:
-              st.error(f"Error loading uploaded image: {str(e)}")
-
+                st.error(f"Error loading uploaded image: {str(e)}")
 
     elif option == "Provide Image URL":
          image_url = st.text_input("Enter Image URL:")
@@ -233,11 +192,6 @@ if selected == "IMAGE CHAT":
              except Exception as e:
                  st.error(f"Error loading image from URL: {str(e)}")
 
-
-
-                
-
-    
     # Use the vision model
     if 'image' in locals():
         try:
@@ -254,36 +208,30 @@ if selected == "IMAGE CHAT":
         except Exception as e:
             st.error(f"An error occurred: {str(e)}")
 
-
-
 if selected == "PDF CHAT":
    # Function to extract text from PDF documents
     def get_pdf_text(pdf_docs):
-     text=""
-     for pdf in pdf_docs:
-        pdf_reader= PdfReader(pdf)
-        for page in pdf_reader.pages:
-            text+= page.extract_text()
-     return  text
-
-
+        text = ""
+        for pdf in pdf_docs:
+            pdf_reader= PdfReader(pdf)
+            for page in pdf_reader.pages:
+                text += page.extract_text()
+        return text
 
     def get_text_chunks(text):
-     # Function to split text into smaller chunks
+        # Function to split text into smaller chunks
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
         chunks = text_splitter.split_text(text)
         return chunks
 
-
     def get_vector_store(text_chunks):
         # Function to create a vector store from text chunks
-        embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+        embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
         vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
         vector_store.save_local("faiss_index")
 
-
     def get_conversational_chain():
-  # Function to load and configure the conversational chain
+        # Function to load and configure the conversational chain
         prompt_template = """
         Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
         provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
@@ -295,13 +243,13 @@ if selected == "PDF CHAT":
 
         model = ChatGoogleGenerativeAI(model="gemini-pro",temperature=0.3)
 
-        prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
+        prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
         chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
 
         return chain
 
     def main1():
-      # Main function for PDF chat functionality
+        # Main function for PDF chat functionality
         st.header("Chat with PDF ")
 
         user_question = st.chat_input("Ask a Question from the PDF Files")
@@ -313,37 +261,33 @@ if selected == "PDF CHAT":
         st.title("Menu:")
         pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
         
-        if st.button("Submit & Process") :
-                    with st.spinner("Processing..."):
-                        raw_text = get_pdf_text(pdf_docs)
-                        text_chunks = get_text_chunks(raw_text)
-                        get_vector_store(text_chunks)
-                        st.success("Done")
-                        pdf_names = [pdf.name for pdf in pdf_docs]
-                        st.session_state["pdf_srchistory"].append(("PDFS UPLOADED", pdf_names))
-                        st.balloons()
-                        
-                        
+        if st.button("Submit & Process"):
+            with st.spinner("Processing..."):
+                raw_text = get_pdf_text(pdf_docs)
+                text_chunks = get_text_chunks(raw_text)
+                get_vector_store(text_chunks)
+                st.success("Done")
+                pdf_names = [pdf.name for pdf in pdf_docs]
+                st.session_state["pdf_srchistory"].append(("PDFS UPLOADED", pdf_names))
+                st.balloons()
 
     def user_input(user_question):
-       # Function to process user input and generate a response
-        embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+        # Function to process user input and generate a response
+        embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
         
         new_db = FAISS.load_local("faiss_index", embeddings)
         docs = new_db.similarity_search(user_question)
 
         chain = get_conversational_chain()
 
+        response1 = chain({"input_documents": docs, "question": user_question}, return_only_outputs=True)
         
-        response1 = chain( {"input_documents":docs, "question": user_question} , return_only_outputs=True)
-        
-        print(response1)
         st.write("Reply: ", response1["output_text"])
         
         output_Text = response1["output_text"]
         st.session_state["pdf_history"].append(("YOU", user_question))
         st.session_state["pdf_history"].append(("PDF_BOT", output_Text))
-     #block will only execute if the script is run directly by the Python interpreter, not if it's imported as a module into another script.
- #
+
+    # Block will only execute if the script is run directly by the Python interpreter, not if it's imported as a module into another script.
     if __name__ == "__main__":
         main1()
