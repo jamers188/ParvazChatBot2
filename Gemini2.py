2171f78ca399468d559cf155aac722498cb617a6
jamers188 <123589840+jamers188@users.noreply.github.com>
2024-03-21 23:02:14 +0400

    Update Gemini2.py

--- Gemini2.py
+++ Gemini2.py
@@ -14,6 +14,8 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from PIL import Image
+
 
 
 # Load environment variables from .env file if present
