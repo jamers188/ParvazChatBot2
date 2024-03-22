cec496049b9a071b99684afae323ce55bd4edfad
jamers188 <123589840+jamers188@users.noreply.github.com>
2024-03-22 07:43:14 +0400

    Update Gemini2.py

--- Gemini2.py
+++ Gemini2.py
@@ -219,10 +219,10 @@ if selected == "IMAGE CHAT":
               st.error(f"Error loading uploaded image: {str(e)}")
 
 
-    elif option == "Provide Image URL (Copy Image address)":
+    elif option == "Provide Image URL":
          st.write("Enter Image URL:")
          with st.form(key='image_url_form'):
-              image_url = st.text_input("Paste Image URL Here:")
+              image_url = st.text_input("Paste Image URL Here(Paste Image address):")
               submitted = st.form_submit_button("Submit URL")
               if submitted and image_url:
                  try:
