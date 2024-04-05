d3ce580297975aff3b00e3c8d5a9d015416186b4
jamers188 <123589840+jamers188@users.noreply.github.com>
2024-04-05 18:35:01 +0400

    Update Gemini2.py

--- Gemini2.py
+++ Gemini2.py
@@ -26,112 +26,65 @@ genai.configure(api_key=API_KEY)
 model = genai.GenerativeModel("gemini-pro")
 chat = model.start_chat(history=[])
 
-from streamlit_option_menu import option_menu
+# Define the main function
+def main():
+    # Initialize session state for chat history, image history, PDF history, etc.
+    if 'chat_history' not in st.session_state:
+        st.session_state['chat_history'] = []
+    if 'img_history' not in st.session_state:
+        st.session_state['img_history'] = []
+    if 'img_srchistory' not in st.session_state:
+        st.session_state['img_srchistory'] = []
+    if 'pdf_history' not in st.session_state:
+        st.session_state['pdf_history'] = []
+    if 'pdf_srchistory' not in st.session_state:
+        st.session_state['pdf_srchistory'] = []
 
-# Initialize session state for chat history, image history, PDF history, etc.
-if 'chat_history' not in st.session_state:
-    st.session_state['chat_history'] = []
-if 'img_history' not in st.session_state:
-    st.session_state['img_history'] = []
-if 'img_srchistory' not in st.session_state:
-    st.session_state['img_srchistory'] = []
-if 'pdf_history' not in st.session_state:
-    st.session_state['pdf_history'] = []
-if 'pdf_srchistory' not in st.session_state:
-    st.session_state['pdf_srchistory'] = []
+    # Set page configuration
+    st.set_page_config(page_title="MyAI", page_icon="🤖")
 
-# Initialize streamlit app
-st.set_page_config(page_title="MyAI", page_icon="🤖")
+    # Define selected option
+    selected_option = st.sidebar.selectbox("Select an Option", ["HOME", "Prompt Chat", "IMAGE CHAT", "PDF CHAT", "CHAT HISTORY"])
 
-# Get the selected option from the sidebar
-selected_option = st.sidebar.selectbox("Select an Option", ["HOME", "Prompt Chat", "IMAGE CHAT", "PDF CHAT", "CHAT HISTORY"])
-
-# Displaying the home page content
-if selected_option == 'HOME':
     # Displaying the home page content
-    st.markdown("""# <span style='color:#0A2647'> Welcome to My Streamlit App  ** MyAI 🦅</span>""", unsafe_allow_html=True)
-    st.markdown("""#### <span style='color:#0E6363'> Based on Gemini-PRO,GEMINI-PRO-Vision LLM API FROM GOOGLE</span>""", unsafe_allow_html=True)
-    st.markdown("""## <span style='color:##11009E'>Introduction</span>""", unsafe_allow_html=True)
-    st.markdown(""" <span style='color:#020C0C'> MyAI is an innovative chatbot application designed to provide intelligent responses to your queries. Powered by advanced language and vision models, it offers a seamless conversational experience for various use cases. </span>""", unsafe_allow_html=True)
-    st.markdown("""
-    ### <span style='color:#0F0F0F'>Instructions:</span>
-    <span style='color:#222831'>  📖 Navigate to Prompt CHAT for the Text based results..</span>
-    <br>
-    <span style='color:#222831'>  📸 Navigate to IMAGE CHAT for the IMAGE based results..</span>
-    <br>
-    <span style='color:#222831'>  📁 Navigate to PDF CHAT to chat with the PDF'S..</span>
-    <br>
-    <br>
-     <span style='color:#222831'> Explore the Possibilities:</span>
-     <br>
-     
-     <span style='color:#222831'> ParvazChatBot2 is a versatile tool that can assist you in various tasks, from answering questions to analyzing images and PDF documents. Explore its capabilities and discover new ways to leverage its intelligence for your needs. 
-     </span>
-    <br>
-""", unsafe_allow_html=True)
+    if selected_option == "HOME":
+        # Display the home page content
+        st.markdown("""# <span style='color:#0A2647'> Welcome to My Streamlit App  ** MyAI 🦅</span>""", unsafe_allow_html=True)
+        st.markdown("""#### <span style='color:#0E6363'> Based on Gemini-PRO, GEMINI-PRO-Vision LLM API FROM GOOGLE</span>""", unsafe_allow_html=True)
+        st.markdown("""## <span style='color:##11009E'>Introduction</span>""", unsafe_allow_html=True)
+        st.markdown(""" <span style='color:#020C0C'> MyAI is an innovative chatbot application designed to provide intelligent responses to your queries. Powered by advanced language and vision models, it offers a seamless conversational experience for various use cases. </span>""", unsafe_allow_html=True)
+        st.markdown("""
+        ### <span style='color:#0F0F0F'>Instructions:</span>
+        <span style='color:#222831'>  📖 Navigate to Prompt CHAT for the Text based results..</span>
+        <br>
+        <span style='color:#222831'>  📸 Navigate to IMAGE CHAT for the IMAGE based results..</span>
+        <br>
+        <span style='color:#222831'>  📁 Navigate to PDF CHAT to chat with the PDF'S..</span>
+        <br>
+        <br>
+         <span style='color:#222831'> Explore the Possibilities:</span>
+         <br>
+         
+         <span style='color:#222831'> ParvazChatBot2 is a versatile tool that can assist you in various tasks, from answering questions to analyzing images and PDF documents. Explore its capabilities and discover new ways to leverage its intelligence for your needs. 
+         </span>
+        <br>
+        """, unsafe_allow_html=True)
 
-elif selected_option == 'CHAT_HISTORY':
-    st.title("CHAT HISTORY")
-    # Create two columns for buttons
-    text_history_button, image_history_button, pdf_history_button = st.columns([1, 1, 1])
-    # Adjust column ratios as needed
-    with text_history_button:
-        # Display chat history for text if the button is clicked
-        if st.button("Show Text Chat History", use_container_width=True):
-            if 'chat_history' in st.session_state and st.session_state['chat_history']:
-                st.subheader("Text Chat History:")
-                for role, text in st.session_state['chat_history']:
-                    if role == "YOU":
-                        st.markdown(f"**{role} 👤**: {text} ")
-                    elif role == "TEXT_BOT":
-                        st.markdown(f"**{role} 🤖**: {text} ")
-            else:
-                st.error("Text Chat History is empty. Start asking questions to build the history.")
-    with image_history_button:
-        # Display image chat history if the button is clicked
-        if st.button("Show Image Chat History", use_container_width=True):
-            for history_type, header_text, emoji in [
-                ('img_history', "Image Chat History:", "👤"),
-                ('img_srchistory', "Image Source", "👤"),
-            ]:
-                history = st.session_state.get(history_type, [])
-                error_message = f"{header_text} is empty. Start asking questions with images to build the history." if "Chat" in header_text else f"{header_text} is empty. Start uploading images to build the history."
-                if history:
-                    st.subheader(header_text)
-                    for role, text in history:
-                        role_prefix = emoji if role in ["YOU", "SOURCE"] else "🤖"
-                        st.markdown(f"**{role} {role_prefix}**: {text}")
-                else:
-                    st.error(error_message)
-    with pdf_history_button:
-        # Display pdf history for image if the button is clicked
-        if st.button("Show PDF Chat History", use_container_width=True):
-            for history_type in ['pdf_history', 'pdf_srchistory']:
-                if history := st.session_state.get(history_type):
-                    title = "PDF Chat History:" if history_type == 'pdf_history' else "PDF Source History:"
-                    st.subheader(title)
-                    for role, user_text in history:
-                        role_prefix = "👤" if role in ["YOU", "PDFS UPLOADED"] else "🤖"
-                        st.markdown(f"**{role} {role_prefix}**:  {user_text} ")
-                else:
-                    st.error(f"{history_type.capitalize()} is empty. Start asking questions with PDFs to build the history.")
-    st.warning("THE CHAT HISTORY WILL BE LOST ONCE THE SESSION EXPIRES")
-
-# Displaying the PDF chat interface
-elif selected_option == "PDF CHAT":
-    st.title("PDF CHAT")
-    # Option to choose between file upload and URL input
-    pdf_option = st.radio("Choose an option", ["Upload PDF", "Input PDF URL"])
-    if pdf_option == "Upload PDF":
-        uploaded_pdfs = st.file_uploader("Upload PDF(s)", accept_multiple_files=True)
-        if uploaded_pdfs:
-            # Process uploaded PDFs
-            process_pdf_upload(uploaded_pdfs)
-    elif pdf_option == "Input PDF URL":
-        pdf_url = st.text_input("Enter PDF URL")
-        if pdf_url:
-            # Process PDF URL
-            process_pdf_url(pdf_url)
+    # Displaying the PDF chat interface
+    elif selected_option == "PDF CHAT":
+        st.title("PDF CHAT")
+        # Option to choose between file upload and URL input
+        pdf_option = st.radio("Choose an option", ["Upload PDF", "Input PDF URL"])
+        if pdf_option == "Upload PDF":
+            uploaded_pdfs = st.file_uploader("Upload PDF(s)", accept_multiple_files=True)
+            if uploaded_pdfs:
+                # Process uploaded PDFs
+                process_pdf_upload(uploaded_pdfs)
+        elif pdf_option == "Input PDF URL":
+            pdf_url = st.text_input("Enter PDF URL")
+            if pdf_url:
+                # Process PDF URL
+                process_pdf_url(pdf_url)
 
 # Function to process PDF upload and generate a response
 def process_pdf_upload(pdf_files):
@@ -173,8 +126,15 @@ def process_pdf_url(pdf_url):
             except Exception as e:
                 st.error(f"Error processing PDF from URL: {str(e)}")
 
+# Function to get PDF text
+def get_pdf_text(pdf_files):
+    raw_text = ""
+    for pdf_file in pdf_files:
+        pdf_reader = PdfReader(pdf_file)
+        for page in pdf_reader.pages:
+            raw_text += page.extract_text()
+    return raw_text
 
-
-# Run the Streamlit app
+# Call the main function when the script is executed
 if __name__ == "__main__":
     main()
